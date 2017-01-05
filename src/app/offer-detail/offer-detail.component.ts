a408bb7d6f63a58b5618a779ad0f42ba2caf5237
Arthur Papailhau <papailhau@gmail.com>
2017-01-05 20:20:52 +0100

    fix checkbox

--- src/app/offer-detail/offer-detail.component.ts
+++ src/app/offer-detail/offer-detail.component.ts
@@ -1,26 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { OffersService } from '../_services/offers.service';
 import { Offer } from '../_model/Offer';
-import { ActivatedRoute, Params }   from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import { ToastService } from '../_services/toast.service';
+import { ProfileService } from '../_services/profile.service';
 
 @Component({
 	selector: 'app-offer-detail',
 	templateUrl: 'offer-detail.component.html',
-	  styles: [`
-		md-card {
-			margin: 20px;
-		}
-		md-card-content {
-			padding-left: 10px;
-		}
-		`]
+	styles: [`
+md-card {
+margin: 20px;
+}
+md-card-content {
+padding-left: 10px;
+}
+`]
 })
 export class OfferDetailComponent {
 
-	@Input() offer : Offer;
-	months = ["January",
+	@Input() offer: Offer;
+	months = ['January',
             "February",
             "March",
             "April",
@@ -31,25 +33,30 @@ export class OfferDetailComponent {
             "September",
             "October",
             "November",
-            "December",];
-    private _showDetails: boolean = false;
-    private _loaded: boolean = false;
+            'December',];
 
+	private _showDetails: boolean = false;
+	private _loaded: boolean = false;
+	private _showApply: boolean = true;
+	private _applied: boolean = true;
 
+	private documents = [];
 
 	ngOnInit(): void {
-		if(!this.offer.id) // dedicated page
+		if (!this.offer.id) {
 			this.showDetails = true;
+			this.showApply = true;
+		}
 	}
 
-	constructor(private offersService: OffersService, private route: ActivatedRoute) { 
+	constructor(private offersService: OffersService, private route: ActivatedRoute, private toastService: ToastService, private profileService: ProfileService) {
 		this.offer = new Offer;
 	}
 
 	set showDetails(show: boolean) {
 		this._showDetails = show;
-		if(show && !this._loaded) {
-			if(this.offer.id)
+		if (show && !this._loaded) {
+			if (this.offer.id)
 				this.getDetailsFromInput();
 			else
 				this.getDetailsFromUrl();
@@ -61,15 +68,36 @@ export class OfferDetailComponent {
 		return this._showDetails;
 	}
 
+	set showApply(show: boolean) {
+		this._showApply = show;
+		if (show && this._applied) {
+			this._applied = false;
+		} else {
+			if (show) {
+				this.toastService.show('Application successfully sent!');
+			} else { // click on APPLY
+				console.log('APPLY clicked');
+				this.profileService.loadCVs().subscribe((result) => {
+					this.documents = JSON.parse(JSON.stringify(result));
+					console.log(result);
+				});
+			}
+		}
+	}
+
+	get showApply(): boolean {
+		return this._showApply;
+	}
+
 	getDetailsFromInput(): void {
 		this.offersService.getOffersDetails(this.offer.id)
-		.subscribe(details => this.offer = details || this.offer);
+			.subscribe(details => this.offer = details || this.offer);
 	}
 
 	getDetailsFromUrl(): void {
 		this.route.params
-		.switchMap((params: Params) => this.offersService.getOffersDetails(+params['id']))
-		.subscribe(details => this.offer = details || this.offer);
+			.switchMap((params: Params) => this.offersService.getOffersDetails(+params['id']))
+			.subscribe(details => this.offer = details || this.offer);
 	}
 
 	getStart(): string {
